9445236837f2e32b7ad5aa08e206bda058c849c8
Scott Reynolds <scottreynolds6294@gmail.com>
2024-08-29 13:21:20 -0400

    Models all implemented

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Post = require('./Posts');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+module.exports = {User, Post, Comment };
+
