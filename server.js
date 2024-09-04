49e6c33041d573eda360e254db9116f843dd87ef
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-03 23:32:48 -0400

    styling completed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
     secret: process.env.SESSION_SECRET,
     cookie: {
-        maxAge: 300000,
+        maxAge: 1000000,
         httpOnly: true,
         secure: false,
         sameSite: 'strict',
