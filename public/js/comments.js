97afbe34ec8993177fe5f303cd93bcd8bb785908
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-05 14:23:35 -0400

    some more styling completed/gitignore issue still persitent

--- public/js/comments.js
+++ public/js/comments.js
@@ -2,20 +2,20 @@ const newCommentHandler = async (event) => {
     event.preventDefault();
 
     const comment_text = document.querySelector('#comment-text').value.trim();
-    const post_id = document.querySelector('#post-id').value;  // Assuming you have a hidden input with the post ID
+    const post_id = document.querySelector('#post-id').value;  
 
     if (comment_text) {
         try {
             const response = await fetch('/api/comments', {
                 method: 'POST',
-                body: JSON.stringify({ comment_text, post_id }), // Include post_id in the request body
+                body: JSON.stringify({ comment_text, post_id }),
                 headers: {
                     'Content-Type': 'application/json',
                 },
             });
 
             if (response.ok) {
-                document.location.replace(`/post/${post_id}`);  // Redirect to the specific post page
+                document.location.replace(`/post/${post_id}`); 
             } else {
                 alert('Failed to create comment: ' + response.statusText);
             }
