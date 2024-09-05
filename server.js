09bb861fecc69bc3a8dd99e95adc31b3fb4e7b18
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-05 18:11:44 -0400

    render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
