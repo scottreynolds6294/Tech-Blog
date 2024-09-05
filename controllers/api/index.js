7faf30635521dcec13389715539df758d91f1d0c
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-05 14:32:41 -0400

    gitignore issue fixed

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./user_routes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
+
+
---
+++ controllers/api/user_routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.logged_in = true;
+
+            res.status(201).json(newUser); 
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'Failed to create user', error: err.message });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect username or password' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'Failed to log in', error: err.message });
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).json({ message: 'No user is logged in' });
+    }
+});
+
+module.exports = router;
