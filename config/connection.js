2e0c4a848374b1b21756208d5560d8070a84042c
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-05 17:42:50 -0400

    render fix

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-const Sequelize = require('sequelize');
+const { Sequelize }= require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
-if(process.env.DB_URL) {
-    sequelize = new Sequelize(process.env.DB_URL);
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: '127.0.0.1',
-            dialect: 'postgres',
-            port: 5432,
-        }
-    );
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+      
+    }
+  );
 }
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const sess = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize,
+        db: sequelize
     })
 };
 
